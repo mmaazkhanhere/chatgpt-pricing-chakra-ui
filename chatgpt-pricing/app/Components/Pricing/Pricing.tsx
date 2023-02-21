dd2265a8bee8ace3b9d6f5e7d0a17606fe0078e3
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-02-21 19:06:28 +0500

    fixing pricing screen responsiveness issues

--- chatgpt-pricing/app/Components/Pricing/Pricing.tsx
+++ chatgpt-pricing/app/Components/Pricing/Pricing.tsx
@@ -6,15 +6,15 @@ export default function Pricing() {
     return (
         <Box maxW='1000px' mx={{ base: '70px', lg: 'auto' }} borderRadius='20px' overflow='hidden' mt='70px' mb='34px'>
 
-            <Flex direction={{ sm: 'column', md: 'column', lg: 'row' }}>
+            <Flex direction={{ base: 'column', md: 'column', lg: 'row' }}>
 
-                <Box bg='rgba(71, 21, 95, 0.3)' p='40px' textAlign='center'>
+                <Box bg='rgba(71, 21, 95, 0.3)' p='30px' textAlign='center'>
 
-                    <Text fontSize={{ sm: '22px', lg: '30px' }} pt='24px'>
+                    <Text fontSize='30px' pt='24px'>
                         ChatGPT Plus
                     </Text>
 
-                    <Heading as='b' fontSize={{ sm: '28px', lg: '60px' }} color='black'>
+                    <Heading as='b' fontSize='60px' color='black'>
                         $20
                     </Heading>
 
@@ -22,7 +22,7 @@ export default function Pricing() {
                         Billed just once
                     </Text>
 
-                    <Button fontWeight='bold' w='261px' h='52px' mt='32px' borderRadius='15px'>
+                    <Button fontWeight='bold' w='241px' h='52px' mt='32px' borderRadius='15px' color='black' bg='white'>
                         Subscribe
                     </Button>
 
@@ -34,18 +34,18 @@ export default function Pricing() {
                         The new subscription plan ChatGPT Plus, will be available for $20/month, and subsrcibers will recieve a number of benefits
                     </Text>
 
-                    <HStack lineHeight='24px'>
-                        <Icon as={arrowMark}></Icon>
+                    <HStack>
+                        <Icon as={arrowMark} />
                         <Text fontSize='18px'>General access to ChatGPT, even during peak times</Text>
                     </HStack>
 
-                    <HStack pt='10px' lineHeight='24px'>
-                        <Icon as={arrowMark}></Icon>
+                    <HStack pt='10px'>
+                        <Icon as={arrowMark} />
                         <Text fontSize='18px'>Faster response times</Text>
                     </HStack>
 
-                    <HStack mt='10px' mb='91px' >
-                        <Icon as={arrowMark}></Icon>
+                    <HStack pt='10px' mb='91px' >
+                        <Icon as={arrowMark} />
                         <Text fontSize='18px'>Priority access to new features and improvements</Text>
                     </HStack>
                 </Box>
