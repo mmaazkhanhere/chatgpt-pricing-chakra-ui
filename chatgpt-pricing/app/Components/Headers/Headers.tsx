f6c2028b1c0ddbdf27f52f5df53b581b1848f410
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-02-21 11:34:26 +0500

    fixing screen response of headers component

--- chatgpt-pricing/app/Components/Headers/Headers.tsx
+++ chatgpt-pricing/app/Components/Headers/Headers.tsx
@@ -3,12 +3,13 @@ import { Box, Heading, Text } from '@chakra-ui/react'
 
 export default function Headers() {
     return (
-        <Box bg='#rgba(71, 21, 95, 1)' textAlign=' center' pt='131px'>
-            <Heading as='b' fontSize='60px' color='white' textAlign='center' >
+        <Box bg='#rgba(71, 21, 95, 1)' pt='131px' textAlign={{ base: 'left', md: 'left', lg: 'center' }} maxW='1900px' >
+
+            <Heading as='b' fontSize='60px' color='white' >
                 Introducing ChatGPT Plus
             </Heading >
 
-            <Text pt='32px' color='#FFFFFF' fontSize='22px' pl='455px' pr='455px' textAlign='center'>
+            <Text pt='32px' color='#FFFFFF' fontSize='22px'>
                 We are launching a pilot subscription plan for ChatGPT, a converstaional AI that can chat with you, answer
                 follow-up questions, and challenge incorrect assumptions
             </Text>
