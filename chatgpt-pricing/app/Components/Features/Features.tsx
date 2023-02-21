bf559e69e28b23ebe414899a0353646c6eae5f51
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-02-21 18:45:03 +0500

    including screen responsiveness in feature comp

--- chatgpt-pricing/app/Components/Features/Features.tsx
+++ chatgpt-pricing/app/Components/Features/Features.tsx
@@ -7,20 +7,20 @@ import payOnce from '../Icons/payOnce'
 
 export default function Features() {
     return (
-        <Box>
-            <Flex>
+        <Box maxW='800px' m='auto' pl='50px'>
+            <Flex direction={{ base: 'column', lg: 'row' }}>
 
-                <HStack>
+                <HStack pl='30px'>
                     <Icon as={globalicon} />
                     <Text>Globally Available</Text>
                 </HStack>
 
-                <HStack>
+                <HStack pl='30px'>
                     <Icon as={bitcoinIcon} />
                     <Text>Can pay in cryptocurrency</Text>
                 </HStack>
 
-                <HStack>
+                <HStack pl='30px'>
                     <Icon as={payOnce} />
                     <Text>Pay once and for all</Text>
                 </HStack>
